19f40958c576dd3ee230b290e0064b14a6795e87
joe-blanca-git <joebblanca@gmail.com>
2026-08-07 16:09:35 -0300

    fix: ajuste url

--- frontend/portal-pat/src/app/core/environments/environment.prod.ts
+++ frontend/portal-pat/src/app/core/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
 
   //=============================PRODUCAO==============================================================
   apiAgivysUrl: 'https://joederblanca.com.br/agivys-api/api/v1/',
-  apiAvivysAuthUrl: 'https://joederblanca.com.br/agivys-api/api/v1/auth/',
+  apiAvivysAuthUrl: 'https://joederblanca.com.br/agivys-api/api/v1/authentication/',
   apiTheosUrl: 'https://portaltheos.com.br/theos-api/api/v1/',
   //===================================================================================================
 };
